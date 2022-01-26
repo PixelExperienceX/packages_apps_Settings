58e0f08705a69087e0c07db5304e52ca9fc1fd43
Emanuele Scarlata <scarlataemanuele@gmail.com>
2022-01-26 13:18:02 +0100

    Settings: Rebrand for Pixel Experience X
    Signed-off-by: Emanuele Scarlata <scarlataemanuele@gmail.com>
    Change-Id: Idb0a4ae39e2c36e613baa5d9dec80401555fb3a8

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -80,10 +80,10 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
     public CharSequence getSummary() {
         StringBuilder sb = new StringBuilder();
         sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
-        String pixelExperienceVersion = VersionUtils.getPixelExperienceVersion();
-        if (!pixelExperienceVersion.equals("")){
+        String pixelExperiencexVersion = VersionUtils.getPixelExperienceXVersion();
+        if (!pixelExperiencexVersion.equals("")){
             sb.append("\n");
-            sb.append(pixelExperienceVersion);
+            sb.append(pixelExperiencexVersion);
         }
         return sb.toString();
     }
